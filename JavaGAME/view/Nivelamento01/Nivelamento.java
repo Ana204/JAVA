bd9e472fd413f8f60ff97dd3b98fafc95b249d67
ana204 <anagomes.lucia@hotmail.com>
2021-06-23 16:59:20 -0300

    criando obj com base numa class e listando as caracteristicas de uma class pessoa

--- JavaGAME/view/Nivelamento01/Nivelamento.java
+++ JavaGAME/view/Nivelamento01/Nivelamento.java
@@ -3,11 +3,17 @@ package view.Nivelamento01;
 import model.NivelamentoPOO1.Pessoa;
 
 public class Nivelamento {
-    
+     
     public static void main(String[] args) {
         
         Pessoa obj = new Pessoa();
 
-        
+        obj.setNome("Ana");
+        obj.setTelefone("9999999");
+        obj.setEmail("Ana@hotmail.com");
+
+        System.out.println("Nome: " + obj.getNome());
+        System.out.println("Telefone: " + obj.getTelefone());
+        System.out.println("Email: " + obj.getEmail());
     }
 }
